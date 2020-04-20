1d8d396e7048b2a73660ad7d3080a9dda9395fb3
任鹏飞 <992430303@qq.com>
2020-04-20 15:02:05 +0800

    feat(init): 初始化仓库

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+/* config-overrides.js */
+const { override, fixBabelImports } = require('customize-cra');
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+);
+
